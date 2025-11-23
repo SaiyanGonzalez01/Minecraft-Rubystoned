1cc741424d823abe4d5b009e1e9cf1c230c52d51
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-11-22 23:51:12 -0500

    Update Block.java

--- src/game/java/net/minecraft/game/level/block/Block.java
+++ src/game/java/net/minecraft/game/level/block/Block.java
@@ -723,7 +723,7 @@ public class Block {
 		var0 = var10000;
 		var0.stepSound = var1;
 		rubystone = var0;
-		var10000 = (new BlockFlower(65, 48)).setHardness(0.0F);
+		var10000 = (new BlockFlower(65, 85)).setHardness(0.0F);
 		var1 = soundGrassFootstep;
 		var0 = var10000;
 		var0.stepSound = var1;
