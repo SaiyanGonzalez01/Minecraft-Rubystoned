60d81ca2bad1958a9a6af3560bc5761cb8ff8a46
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-11-23 21:29:22 -0500

    Create BlockClay.java

---
+++ src/game/java/net/minecraft/game/level/block/BlockClay.java
@@ -0,0 +1,17 @@
+package net.minecraft.game.level.block;
+
+import net.minecraft.game.level.material.Material;
+
+public final class BlockClay extends Block {
+	public BlockClay(int var1, int var2, String var3) {
+		super(69, 53, Material.ground, var3);
+	}
+
+  public int idDropped(int var1, Random var2) {
+		return Item.clay.shiftedIndex;
+	}
+
+  public int quantityDropped(Random var1) {
+		return 4;
+	}
+}
