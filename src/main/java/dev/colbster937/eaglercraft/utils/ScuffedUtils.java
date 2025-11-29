bc31989fb440a59abbeab86fe9ab5accf4d1acdb
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-11-29 13:30:16 -0500

    add time foramtter

--- src/main/java/dev/colbster937/eaglercraft/utils/ScuffedUtils.java
+++ src/main/java/dev/colbster937/eaglercraft/utils/ScuffedUtils.java
@@ -53,4 +53,12 @@ public class ScuffedUtils {
   public static void showZipFileChooser() {
     EagRuntime.displayFileChooser("application/zip", ".zip");
   }
+
+  public static String getFormattedTime(long ticks) {
+    long t = (((ticks % 24000) + 24000) % 24000 + 6000) % 24000;
+    long h = t / 1000;
+    long m = (t % 1000) * 60 / 1000;
+    return HString.format("The day is %s at %s", ticks / 24000,
+        HString.format("%02d:%02d", h, m));
+  }
 }
